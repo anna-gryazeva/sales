621a089c26d214ada31bc752b2d9a5055fa58c29
Anna Gryazeva <anna_gryazeva@mail.ru>
2023-11-11 13:15:45 +0300

    Initial commit

---
+++ src/main/java/ru/netology/stats/StatusService.java
@@ -0,0 +1,74 @@
+package ru.netology.stats;
+
+public class StatusService {
+
+    public long amountSales(long[] sales) {
+        long allSales = 0;
+        for (long sale : sales) {
+            allSales += sale;
+
+        }
+        return allSales;
+    }
+
+
+    public long avrgSales(long[] sales) {
+        long allSales = 0;
+        for (long sale : sales) {
+            allSales += sale;
+        }
+
+        long avrgSales = allSales / 12;
+        return avrgSales;
+
+    }
+
+    public long maxSales(long[] sales) {
+        long maxMonth = 0;
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] >= sales[(int) maxMonth]) {
+                maxMonth = i;
+            }
+        }
+        return maxMonth + 1;
+    }
+
+    public long minSales(long[] sales) {
+        long minMonth = 0;
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] <= sales[(int) minMonth]) {
+                minMonth = i;
+            }
+        }
+        return minMonth + 1;
+    }
+
+    public int minAvrgSales(long[] sales) {
+        int countMonth = 0;
+        long avrgSales = avrgSales(sales);
+        for (long sale : sales) {
+            if (sale < avrgSales) {
+                countMonth++;
+            }
+        }
+            return countMonth;
+        }
+
+        public int maxAvrgSales(long[] sales) {
+        int countMonth = 0;
+        long avrgSales = avrgSales(sales);
+            for (long sale : sales) {
+                if (sale > avrgSales) {
+                    countMonth++;
+            }
+
+            } return countMonth;
+        }
+
+
+}
+
+
+
---
+++ src/test/java/ru/netology/stats/StatusServiceTest.java
@@ -0,0 +1,78 @@
+package ru.netology.stats;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StatusServiceTest {
+
+    @Test
+    public void shouldFindAllSales() {
+        StatusService service = new StatusService();
+
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long expectedSales = 8 + 15 + 13 + 15 + 17 + 20 + 19 + 20 + 7 + 14 + 14 + 18;
+        long actualSales = service.amountSales(sales);
+
+        Assertions.assertEquals(expectedSales, actualSales);
+    }
+
+    @Test
+    public void shouldFindAvrgSales() {
+        StatusService service = new StatusService();
+
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long expectedSales = (8 + 15 + 13 + 15 + 17 + 20 + 19 + 20 + 7 + 14 + 14 + 18) / 12;
+        long actualSales = service.avrgSales(sales);
+
+        Assertions.assertEquals(expectedSales,actualSales);
+
+    }
+
+    @Test
+    public void shouldFindMaxSales() {
+        StatusService service = new StatusService();
+
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long expectedMonth = 8;
+        long actualMonth = service.maxSales(sales);
+        Assertions.assertEquals(expectedMonth, actualMonth);
+    }
+
+    @Test
+    public void shouldFindMinSales() {
+        StatusService service = new StatusService();
+
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long expectedMonth = 9;
+        long actualMonth = service.minSales(sales);
+
+        Assertions.assertEquals(expectedMonth, actualMonth);
+
+    }
+    @Test
+    public void shouldFindMinAvrgSales() {
+        StatusService service = new StatusService();
+
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long expectedMonth = 5;
+        long actualMonth = service.minAvrgSales(sales);
+
+        Assertions.assertEquals(expectedMonth, actualMonth);
+    }
+    @Test
+    public void shouldFindMaxAvrgSales() {
+        StatusService service = new StatusService();
+
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long expected = 5;
+        long actual = service.maxAvrgSales(sales);
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
